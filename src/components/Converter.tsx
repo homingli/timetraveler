84cac122e5d0c4a9e688b95163b44c0c70a8eaa9
Ho Ming Li <homingli@gmail.com>
2026-04-28 17:31:46 +0800

    refactor: simplify time picker and improve layout

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -63,7 +63,7 @@ export const Converter = () => {
       </div>
 
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {mounted && targetZones.map((zone) => {
           const converted = convertTimezone(baseTime, zone);
           const timeStr = formatTime(converted);
